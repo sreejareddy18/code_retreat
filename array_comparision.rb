aa99a121c9648f1b72c8f39ce6f9085decc7e6d4
Sreeja Reddy <Sreeja.Reddy@Cerner.com>
2016-10-14 17:15:37 +0530

    optimise array_comparision

--- array_comparision.rb
+++ array_comparision.rb
@@ -1,8 +1,4 @@
 new_clients = ['app', 'florida']
 existing_clients = ['adventist', 'app']
 
-if (new_clients - existing_clients).empty? || (existing_clients - new_clients).empty?
-  p true
-else
-  p false
-end
+((new_clients - existing_clients).empty? || (existing_clients - new_clients).empty?) ? (p true) : (p false)
